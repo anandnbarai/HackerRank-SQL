6a9f7a63fb7a66051f8fc66d34031f9bd00def51
Anand Barai <ab20892@gmail.com>
2024-12-25 20:00:45 +0530

    Add files via upload

--- Solution/Weather_Observation_Station_13.sql
+++ Solution/Weather_Observation_Station_13.sql
@@ -1,9 +1,9 @@
 --> Weather Observation Station 13
+--> Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345 Truncate your answer to 4 decimal places.
 
 SELECT 
-    ROUND(SUM(lat_n),4)
-FROM
-    Station
-WHERE
-    lat_n > 38.7880 AND 
-    lat_n < 137.2345;
+    ROUND(CAST(SUM(LAT_N) AS DECIMAL(10,4)), 4) 
+FROM 
+    STATION  
+WHERE 
+    LAT_N BETWEEN 38.7880 AND 137.2345;
